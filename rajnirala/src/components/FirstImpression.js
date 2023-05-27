70f2ebb987799e5d9149b5e1549218d809497681
Raj Nirala <rajnirala4104@gmail.com>
2023-05-27 18:16:48 +0530

    litbit changes

--- rajnirala/src/components/FirstImpression.js
+++ rajnirala/src/components/FirstImpression.js
@@ -1,8 +1,22 @@
+import './css/FirstImpression.css'
+
 function FirstImpression(props) {
-    return(
+    return (
         <>
             <div className="firstImpressionContainer">
-                <h1>This is Raj Nirala</h1>
+                <div className="firstImpressionLeftSectionContent">
+                    <div className="liBitAboutMe">
+                        <span>Hey, Hii..</span>
+                        <span>I'm a Fullstack Developer</span>
+                    </div>
+                    <div className="firstImpressionLeftSectionBtns">
+                    <button type="button" class="btn btn-secondary btn-sm">Let's Work</button>
+                    <button type="button" class="btn btn-outline-secondary btn-sm">Download CV</button>
+                    </div>
+                </div>
+                <div className="firstImpressionRightSectionContent">
+                    <div className="myImage"></div>
+                </div>
             </div>
         </>
     )
