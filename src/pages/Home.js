694c76c33e08c4a50c70da5fe611cd42f4d488f9
Raj Nirala <rajnirala4104@gmail.com>
2023-06-11 14:57:19 +0530

    organise folder structure

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import '../App.css';
+import FirstImpression from '../components/FirstImpression';
+import NavBar from "../shared/NavBar";
+import { useLocation } from "react-router-dom";
+
+export const Home = ()=>{
+
+    const location = useLocation();
+    return (
+        <div className="App">
+            <NavBar activeStatus={location.pathname}/>
+            <div className="myWebAppContainer">
+                {<FirstImpression />}
+            </div>
+        </div>
+    );
+}
+
