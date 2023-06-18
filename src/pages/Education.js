726b7c51deb0b7e4ad8a1c3cf24122e1c5c259a1
Raj Nirala <rajnirala4104@gmail.com>
2023-06-18 18:48:13 +0530

    added aducation page error

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -6,7 +6,7 @@ export const Education = (props)=>{
     return(
         <>
             {/* <NavBar activeStatus={location.pathname}/> */}
-            My Education 
+            <div className="container d-flexjustify-content-center shadow col-8 my-4 rounded text-center bg-light text-danger display-4">Oops!! <br />Abhi kaam chal raha hai </div>
         </>
     )
 }
