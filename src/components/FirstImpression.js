75f1a930ec29e55b0b3a8df27e14afa9b05e9f0a
Raj Nirala <rajnirala4104@gmail.com>
2023-06-18 18:08:50 +0530

    added typed.js for text animation

--- src/components/FirstImpression.js
+++ src/components/FirstImpression.js
@@ -1,8 +1,27 @@
 import { Link } from 'react-router-dom'
 import '../pages/css/FirstImpression.css'
+import React from 'react';
+import Typed from 'typed.js';
 
 
 export default function FirstImpression(props) {
+
+
+    // Create reference to store the DOM element containing the animation
+    const el = React.useRef(null);
+
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['<i>Javascript</i> Enthusiast', "<i>Python</i> Enthusiast", " <i>Great</i> Learner" , "<i>Backend</i> Developer", 'FullStack Developer'],
+            typeSpeed: 55,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
+
+
     return (
         <>
             <div className="firstImpressionContainer">
@@ -10,7 +29,7 @@ export default function FirstImpression(props) {
                     <div className="liBitAboutMe">
                         <span className='hi'>Hii<hr /></span>
                         <span className='me'>I'm<span> Raj Nirala</span></span>
-                        <span className='developer'>A Fullstack Developer</span>
+                        <span className='developer'><span ref={el}></span></span>
                     </div>
                     <div className="fristImpressionSocialIcons">
                         <a href="https://www.linkedin.com/in/raj-nirala/" target="_blank" rel="noopener noreferrer">
