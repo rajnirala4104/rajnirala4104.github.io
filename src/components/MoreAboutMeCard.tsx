73bdf3436eccee1e432ecbf95d948d50bf77c524
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2024-12-13 13:15:12 +0530

    lil bit changes

--- src/components/MoreAboutMeCard.tsx
+++ src/components/MoreAboutMeCard.tsx
@@ -7,19 +7,19 @@ const MoreAboutMeCard: React.FC<MoreAboutMeCardInterface> = (props) => {
          <Suspense fallback="loading..">
             {/* ---------- large screen ---------- */}
             <div
-               className="hidden lg:bg-local bg-fixed rounded-md shadow-lg p-2 mx-1  h-[35rem] w-[96%] lg:flex justify-center items-center mb-3 saturate-[100%] transition-all duration-200 lg:hover:saturate-150"
+               className="hidden lg:bg-local bg-fixed rounded-md shadow-lg mx-1  h-[35rem] w-[96%] lg:flex justify-center items-end mb-3 saturate-[100%] transition-all duration-200 lg:hover:saturate-150"
                style={{
-                  background: `linear-gradient(rgba(0, 0, 0, 0.094), rgba(0, 0, 0, 0.924)), url(${props.cardImg}) center/cover`,
+                  background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1.6)), url(${props.cardImg}) center/cover`,
                   boxShadow: "4px 6px 12px -6px black",
                }}
             >
-               <div className="cardContent text-white w-[80%]  mt-10">
+               <div className="cardContent text-white h-[40%] p-5 flex flex-col justify-center items-start w-full bg-gradient-to-b from-transparent to-black rounded-md">
                   <div className="cardTitle my-2">
                      <span className="font-semibold text-[27px]">
                         {props.cardTitle}
                      </span>
                   </div>
-                  <div className="cardDescription text-[15px]">
+                  <div className="cardDescription text-[15px] w-[80%]">
                      {props.cardDescription}
                   </div>
                </div>
@@ -27,14 +27,14 @@ const MoreAboutMeCard: React.FC<MoreAboutMeCardInterface> = (props) => {
 
             {/* ---------- small screen ---------- */}
             <div
-               className="lg:hidden bg-fixed rounded-md shadow-lg p-2 mx-1  h-[35rem] w-[96%] flex justify-center items-center mb-3 saturate-[100%] transition-all duration-200 lg:hover:saturate-150"
+               className="lg:hidden bg-fixed rounded-md shadow-lg mx-1  h-[35rem] w-[96%] flex justify-center items-end mb-3 saturate-[100%] transition-all duration-200 lg:hover:saturate-150"
                style={{
-                  background: `linear-gradient(rgba(0, 0, 0, 0.094), rgba(0, 0, 0, 0.924)), url(${props.cardImg}) center/cover`,
+                  background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1.6)), url(${props.cardImg}) center/cover`,
                   backgroundAttachment: "fixed",
                   boxShadow: "4px 6px 12px -6px black",
                }}
             >
-               <div className="cardContent text-white w-[80%] lg:">
+               <div className="cardContent text-white h-[50%] p-5 flex flex-col justify-center items-start w-full bg-gradient-to-b from-transparent to-black rounded-md ">
                   <div className="cardTitle my-2">
                      <span className="font-semibold text-[27px]">
                         {props.cardTitle}
